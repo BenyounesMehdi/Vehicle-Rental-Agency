aefa2413a86c4c6828e9c01cb29a878f23d6105a
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-02-19 14:17:58 +0100

    ADD The Option To Switch Between The (Light|Night) Mode

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -90,6 +90,13 @@
                     <span class="ms-3 whitespace-nowrap">Brands</span>
                     </a>
                 </li>
+                <li>
+                    <button id="theme-toggle" type="button" class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg id="theme-toggle-dark-icon" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                        <svg id="theme-toggle-light-icon" class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                        <span id="themeExepression" class="ms-3 whitespace-nowrap"></span>
+                    </button>
+                </li>
                 <li class="absolute bottom-2" style="width: 90%;">
                     <a href="./signOut.php" class="w-full flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
@@ -98,7 +105,6 @@
                     <span class="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
                     </a>
                 </li>
-               
             </ul>
         </div>
         </aside>
@@ -214,6 +220,7 @@
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
     <script>
         function themeToggle() {
+            var themeExepression = document.getElementById("themeExepression");
             var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
             var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
 
@@ -224,8 +231,12 @@
                     window.matchMedia("(prefers-color-scheme: dark)").matches)
             ) {
                 themeToggleLightIcon.classList.remove("hidden");
+                themeExepression.innerHTML = "Night Mode" ;
+                
             } else {
                 themeToggleDarkIcon.classList.remove("hidden");
+                themeExepression.innerHTML = "Light Mode" ;
+                
             }
 
             var themeToggleBtn = document.getElementById("theme-toggle");
@@ -240,9 +251,11 @@
                     if (localStorage.getItem("color-theme") === "light") {
                         document.documentElement.classList.add("dark");
                         localStorage.setItem("color-theme", "dark");
+                        themeExepression.innerHTML = "Night Mode" ;
                     } else {
                         document.documentElement.classList.remove("dark");
                         localStorage.setItem("color-theme", "light");
+                        themeExepression.innerHTML = "Light Mode" ;
                     }
                 }
                 // if NOT set via local storage previously
