89d25b328b2192a561b8def08f3f9e8d7d38ceb6
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-02-19 23:30:31 +0100

    ADD a Page For Each Dashboard Item

---
+++ views/admin/vehiclesSide.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+    <p class="text-2xl dark:text-white">Vehicles</p>
+
+</body>
+</html>
