33e4d42f22748a0fdae0c7e48bf97f5eef445e3a
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-02-21 00:11:19 +0100

    UPD

--- views/JS/main.js
+++ views/JS/main.js
@@ -32,8 +32,9 @@ function toggle() {
 }
 
 
+
 function theme () {
-    console.log("Theme function called");
+    // console.log("Theme function called");
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark');
@@ -43,7 +44,8 @@ function theme () {
 }
 
 function themeToggle () {
-    console.log("Theme toggle function called");
+    // console.log("Theme toggle function called");
+    // theme() ;
     var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
     var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
