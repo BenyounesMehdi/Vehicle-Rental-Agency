62e8e3630c3871f875aa8fea9e4ed3848e555348
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-02-27 00:12:21 +0100

    UPD The UI

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -75,7 +75,7 @@
                 else { // if the Admin is already Logged in
                     ?> 
                 
-                        <a href="./views/admin/dashboard.php" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <a href="./views/admin/dashboardSide.php" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Go To Dashboard
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
