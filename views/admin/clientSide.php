aaccd863e8672b51d7a11550b8d1f9ebac1ed0d6
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-01 16:50:06 +0100

    Show The Total Number Of The Table

--- views/admin/clientSide.php
+++ views/admin/clientSide.php
@@ -1,5 +1,7 @@
 <?php
     session_start() ;
+    require_once '../../models/database.php' ;
+    
 ?>
 
 <!DOCTYPE html>
@@ -9,38 +11,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../node_modules/flowbite/dist/flowbite.min.css">
+
+
 </head>
 <body>
+    
 
 
-    <?php include '../components/sidebar.php' ; ?>
+        <?php include '../components/sidebar.php' ; ?>
+
+        <?php 
+            include '../../models/functions.php' ; 
+            $tableName = "client" ;
+        ?>
         
         <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 h-screen" id="content-container">
-            <div class="flex justify-end items-end mb-2 mr-3">
-                <a href="../brands/addbrand.php" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Add a Client
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
-            </div>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">Total Number : <?php echo countRableRows($pdo, $tableName) ?></button>
+            <?php include '../clients/clientsList.php' ; ?>
             
         </div>
 
-    
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
     <script src="../JS/themeToggle.js"></script>
-    <script src="../JS/arrowToggle.js"></script>
+    <script src="../JS/clientsSearch.js"></script>
     <script>
+            
         
         function showSearchButtonToggole () {
             const searchInputValue = document.getElementById("default-search").value ;
             const searchButton = document.getElementById("searchButton") ;
+            const clientsTable = document.getElementById("clientsTable") ;
+            const clientsList = document.getElementById("clientsList") ;
+            
             if( searchInputValue.length > 0 ) {
-                searchButton.classList.remove("hidden") ;
+                // searchButton.classList.remove("hidden") ;
+
+                clientsList.classList.remove("hidden") ;
+                clientsTable.classList.add("hidden") ;
             }
             else {
-                searchButton.classList.add("hidden") ;
+                // searchButton.classList.add("hidden") ;
+
+                clientsList.classList.add("hidden") ;
+                // brandsTable.classList.remove("hidden") ;
             }
         }
 
--- views/admin/vehiclesTypeSide.php
+++ views/admin/vehiclesTypeSide.php
@@ -42,11 +42,16 @@
     
 
         <?php include '../components/sidebar.php' ; ?>
+        <?php 
+            include '../../models/functions.php' ; 
+            $tableName = "vehiclestype" ;
+        ?>
         
         <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 h-screen" id="content-container">
-            <div class="flex justify-end items-end mb-2 mr-3">
+            <div class="flex justify-between items-center mb-2 mr-3 ">
+                <button type="button" class="mt-2 text-white bg-blue-700 hover:bg-blue-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700">Total Number : <?php echo countRableRows($pdo, $tableName) ?></button> 
                 <a href="../vehiclesType/addVehicleType.php" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Add a Vehicle Type
+                        Add a Client
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
