4d2609bf4cbabc41bb3539a892fa5c5ed4c84672
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-01 14:36:08 +0100

    UPD The UI

--- views/vehiclesType/vehiclesTypeList.php
+++ views/vehiclesType/vehiclesTypeList.php
@@ -83,7 +83,7 @@ if (!empty($searchQuery)) {
                                     <?php echo $vehicleType->name; ?>
                                 </td>
                                 <td class="px-6 py-2">
-                                    <a href="../brands/editBrand.php?id=<?php echo $vehicleType->brandID ?>">
+                                    <a href="../vehiclesType/editVehicleType.php?id=<?php echo $vehicleType->vehiclesTypeID ?>">
                                         <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 ">Edit</button>    
                                     </a>
                                     <a data-modal-target="popup-modal" data-modal-toggle="popup-modal" >
