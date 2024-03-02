33c3a860505b5ee5522438ad0958827caca83128
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-02 23:30:46 +0100

    ADD The Cards Section

--- views/admin/dashboardSide.php
+++ views/admin/dashboardSide.php
@@ -1,5 +1,7 @@
 <?php
     session_start() ;
+    include_once '../../models/database.php' ;
+    include_once '../../models/functions.php'
 ?>
 
 <!DOCTYPE html>
@@ -16,15 +18,10 @@
         <?php include '../components/sidebar.php' ; ?>
         
         <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 h-screen" id="content-container">
-            <div class="flex justify-end items-end mb-2 mr-3">
-                <a href="../brands/addbrand.php" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Dashboard Side  
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
-            </div>
-            
+            <p class="mb-3 text-2xl font-semibold text-gray-900 dark:text-white">Dashboard</p>
+
+           <?php include '../components/dashboardCardsSection.php' ?>
+
         </div>
 
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
