a21ec332fb8fba1661d3eeb91ad4c7a96394d634
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-07 17:34:39 +0100

    ADD The getData Function For Fetching All The Data From Any Table

--- models/functions.php
+++ models/functions.php
@@ -15,4 +15,18 @@
         return $rowCount;
     }
 
+    function getData ($pdo, $tableName) {
+        // Prepare the SQL query
+        $query = "SELECT * FROM $tableName";
+
+        // Prepare and execute the statement
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+
+        // Fetch the result
+        $data = $statement->fetchAll();
+
+        return $data;
+    }
+
 ?>
