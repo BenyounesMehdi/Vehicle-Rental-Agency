31f4a4a11cf2c3610d42685864211163d23fd5ac
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-22 18:43:12 +0100

    UPD Hide The Reserve Button For The Reserved vehicles

--- views/components/vehiclesCarousel.php
+++ views/components/vehiclesCarousel.php
@@ -167,9 +167,11 @@
                                               if( !isset($_SESSION['admin']) ) {
                                                 // The client should be singed in to go to the reservation page
                                                 if( isset($_SESSION['client']) ) {
+                                                   if( $vehicle->vehicleStatus == 'Available' ) {
                                                     ?>
                                                       <a href="../../../VehicleRentalAgency/views/client/reservation.php?id=<?php echo $_SESSION['client']->clientID; ?>&vehicle=<?php echo $vehicle->vehicleID; ?>" class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg text-sm px-5 py-2.5 me-2 mt-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> Reserve Now </a>
                                                     <?php
+                                                   }
                                                 }
                                                 else {
                                                     ?>
