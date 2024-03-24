2befa592b42d6e772a793b339a1fb7390e1ae88f
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-24 16:01:22 +0100

    Working On The UI

---
+++ views/components/opinionsCarousel.php
@@ -0,0 +1,49 @@
+<div class="container mx-auto  relative mb-10 p-4">
+
+
+    <div id="indicators-carousel" class="w-full" data-carousel="static">
+        <!-- Carousel wrapper -->
+        <div class="relative h-56 overflow-hidden rounded-lg md:h-96  p-2">
+            <!-- Item 1 -->
+            <div class="hidden overflow-y-auto duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item="active">
+                <div class="flex justify-center items-center mt-10">
+                    <img src="./assets/clientProfile/unknownPP.jpg" class="md:w-20 w-14 rounded-full object-cover">
+                </div>
+                <p class="text-center mt-2 font-semibold md:text-xl">Benyounes Mehdi</p>
+                <p class="text-center mt-3 px-3 font-semibold italic leading-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi unde nam minus cum eaque accusantium, saepe veniam perspiciatis architecto optio dolore sequi temporibus nostrum obcaecati deleniti suscipit a id cupiditate, doloribus blanditiis? Eos expedita magni porro aspernatur labore mollitia eveniet tempore esse, debitis molestias provident soluta illum nesciunt officiis architecto dolorem deleniti voluptatem vero necessitatibus sequi? Recusandae quasi ipsum reiciendis eaque provident quod nobis vero obcaecati nemo corporis, numquam possimus debitis fuga! Consectetur corrupti exercitationem quia fuga quo voluptas ipsam architecto minus odio assumenda molestiae adipisci dolorem voluptatibus debitis quae nihil quasi nobis, optio reiciendis? Sed voluptatem similique nostrum accusamus?</p>
+            </div>
+            <!-- Item 2 -->
+            <div class="hidden duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item>
+            <p class="text-center mt-5 font-semibold text-xl">Benyounes Mohammed Anis</p>
+            </div>
+            <!-- Item 3 -->
+            <div class="hidden duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item>
+            <p class="text-center mt-5 font-semibold text-xl">Debbi Abdelhak</p>
+            </div>
+            <!-- Item 4 -->
+            <div class="hidden duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item>
+            <p class="text-center mt-5 font-semibold text-xl">Drif Mohammed Amine</p>
+            </div>
+        </div>
+        
+        <!-- Slider controls -->
+        <div class=" mt-10 flex justify-center gap-0"> 
+            <button type="button" class="relative top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span class="inline-flex items-center shadow-md border-2 justify-center w-14 h-14 rounded-full bg-white ">
+                    <i id="left" class="fa-solid fa-angle-left font-bold text-2xl"></i>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button" class="relative top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span class="inline-flex items-center shadow-md border-2 justify-center w-14 h-14 rounded-full bg-white ">
+                    <i id="left" class="fa-solid fa-angle-right font-bold text-2xl"></i>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+        </div>
+        
+        
+    </div>
+
+</div>
