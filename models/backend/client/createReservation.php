389dc62931a7b8b37e747a0e7a9cca9b040bc59c
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-03-28 15:15:56 +0100

    ADD The isExpired Attribute In the reservation Table

--- models/backend/client/createReservation.php
+++ models/backend/client/createReservation.php
@@ -7,16 +7,17 @@
         $totalCost = $_POST['totalCost'] ;
         $reservationDate = date('Y-m-d');
         $isPayed = 'No';
+        $isExpired = 'No';
         $clientID = $_POST['clientID'] ;
         $vehicleID = $_POST['vehicleID'] ;
 
         require_once '../../database.php' ;
         
         $query = 'INSERT INTO reservation
-        (pickupDate, returnDate, duration, totalCost, reservationDate, isPayed, clientID, vehicleID)
-         VALUES (?,?,?,?,?,?,?,?)';
+        (pickupDate, returnDate, duration, totalCost, reservationDate, isPayed, isExpired, clientID, vehicleID)
+         VALUES (?,?,?,?,?,?,?,?,?)';
         $stmt = $pdo->prepare($query);
-        $inserted = $stmt->execute([$pickupDate, $returnDate, $duration, $totalCost, $reservationDate, $isPayed, $clientID, $vehicleID]);
+        $inserted = $stmt->execute([$pickupDate, $returnDate, $duration, $totalCost, $reservationDate, $isPayed, $isExpired, $clientID, $vehicleID]);
 
         if( $inserted ) {
             // Modify the status of the reserved vehicle
