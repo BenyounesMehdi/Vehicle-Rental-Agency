7940aeeb9e4513fdce1caaa5080095cf6d7230dc
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-04-02 16:23:59 +0200

    Working on The Edit Process

---
+++ models/backend/client/editOpinion.php
@@ -0,0 +1,30 @@
+<?php
+    echo "<pre>" ;
+    print_r($_POST) ;
+    echo "</pre>" ;
+
+    // if( isset($_POST['editReservation']) ) {
+    //     $reservationID = $_POST['reservationID'] ;
+    //     $pickupDate = date('Y-m-d', strtotime($_POST['pickupDate']))  ;
+    //     $returnDate = date('Y-m-d', strtotime($_POST['returnDate'])) ;
+    //     $duration = $_POST['duration'] ;
+    //     $totalCost = $_POST['totalCost'] ;
+    //     $reservationDate = date('Y-m-d');
+    //     $isPayed = 'no';
+
+    //     require_once '../../database.php' ;
+        
+    //     $query = "UPDATE reservation 
+    //       SET pickupDate = ?, returnDate = ?, duration = ?, totalCost = ?
+    //       WHERE reservationID = ?";
+    //       $stmt = $pdo->prepare($query);
+    //       $updated = $stmt->execute([$pickupDate, $returnDate, $duration, $totalCost, $reservationID]);
+
+    //     if( $updated ) {
+    //         echo "Done" ;
+    //         header( 'location: ../../../views/client/myReservations.php' ) ;
+    //     }
+    //     else {
+    //         echo "Fail" ;
+    //     }
+    // }
