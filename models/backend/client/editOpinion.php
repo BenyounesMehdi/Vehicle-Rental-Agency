f20fec9897cee0c7f82515d6ef0a27c77abb89eb
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-04-02 17:54:14 +0200

    Finished The Update Process

--- models/backend/client/editOpinion.php
+++ models/backend/client/editOpinion.php
@@ -2,29 +2,26 @@
     echo "<pre>" ;
     print_r($_POST) ;
     echo "</pre>" ;
-
-    // if( isset($_POST['editReservation']) ) {
-    //     $reservationID = $_POST['reservationID'] ;
-    //     $pickupDate = date('Y-m-d', strtotime($_POST['pickupDate']))  ;
-    //     $returnDate = date('Y-m-d', strtotime($_POST['returnDate'])) ;
-    //     $duration = $_POST['duration'] ;
-    //     $totalCost = $_POST['totalCost'] ;
-    //     $reservationDate = date('Y-m-d');
-    //     $isPayed = 'no';
-
-    //     require_once '../../database.php' ;
+    
+    if( isset($_POST['editOpinion']) ) {
+        $opinionID = $_POST['opinionID'] ;
+        $opinionContent = $_POST['opinionContent'] ;
+        $rating = $_POST['rating'] ;
+        require_once '../../database.php' ;
         
-    //     $query = "UPDATE reservation 
-    //       SET pickupDate = ?, returnDate = ?, duration = ?, totalCost = ?
-    //       WHERE reservationID = ?";
-    //       $stmt = $pdo->prepare($query);
-    //       $updated = $stmt->execute([$pickupDate, $returnDate, $duration, $totalCost, $reservationID]);
+        $query = "UPDATE opinion 
+          SET content = ?, rating = ?
+          WHERE opinionID = ?";
+          $stmt = $pdo->prepare($query);
+          $updated = $stmt->execute([$opinionContent, $rating, $opinionID]);
 
-    //     if( $updated ) {
-    //         echo "Done" ;
-    //         header( 'location: ../../../views/client/myReservations.php' ) ;
-    //     }
-    //     else {
-    //         echo "Fail" ;
-    //     }
-    // }
+        if( $updated ) {
+            //Redirect the user to the previous page
+            // echo '<script>window.history.go(-2);</script>';
+            header("Location: ../../../index.php");
+        
+        }
+        else {
+            echo "Fail" ;
+        }
+    }
