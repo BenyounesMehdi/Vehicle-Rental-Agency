12cdf68d001946f6d8a6c8dcc96575f89bbc64aa
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-04-02 15:33:26 +0200

    UPD Make The Carousel Dynamic From The Database

--- views/components/opinionsCarousel.php
+++ views/components/opinionsCarousel.php
@@ -1,3 +1,18 @@
+<?php 
+    include './models/database.php' ;
+
+    $query = "SELECT o.*, c.firstName as firstName, c.lastName as lastName
+        FROM opinion o
+        JOIN client c
+        ON o.clientID = c.clientID" ;
+
+        $stmt = $pdo->prepare($query);
+        $stmt->execute(); 
+        $opinions = $stmt->fetchAll() ;
+
+    // var_dump($opinions) ;
+?>
+
 <div id="opinions" class="container mx-auto  relative mb-10 p-4">
 
     <p class="dark:text-white font-semibold text-4xl text-center mb-7">What people are saying about us</p>
@@ -7,54 +22,37 @@
         <!-- Carousel wrapper -->
         <div class="relative h-56 overflow-hidden rounded-lg md:h-96  p-2">
 
-        <div class="hidden  overflow-y-auto duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item="active">
-                <div class="flex justify-center items-center mt-10">
-                    <img src="./assets/clientProfile/unknownPP.jpg" class="md:w-20 w-14 rounded-full object-cover">
-                </div>
-                <p class="text-center mt-5 font-semibold md:text-xl dark:text-white">Benyounes Mehdi</p>
-                <p class="text-center mt-5 px-3 font-semibold italic leading- dark:text-white"> <span class="text-gray-400 dark:text-gray-600 text-3xl">&ldquo;</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officia deserunt repellendus ducimus ipsam ut itaque beatae mollitia quod ratione atque vero reiciendis dolorem dolor facilis eum aliquid illum, aperiam optio tempora, ullam maxime eos? Accusamus vel id ad ipsum repellat aperiam nisi, minima laudantium debitis pariatur quibusdam provident totam! <span class="text-gray-400 dark:text-gray-600 text-3xl">&rdquo;</span></p>
-                    <div class="flex mt-4 md:mt-5 lg:mt-10 mb-5 justify-center gap-1">
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                    </div>
-            </div>
+            
+            <?php 
+                
+                foreach ($opinions as $opinion) { 
+                    $ratings = $opinion->rating;
+                    $i = 0 ;
+                    ?>
+                    <div class="hidden  overflow-y-auto duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item="active">
+                    
+                        <div class="flex justify-center items-center mt-10">
+                            <img src="./assets/clientProfile/unknownPP.jpg" class="md:w-20 w-14 rounded-full object-cover">
+                        </div>
+                        <p class="text-center mt-5 font-semibold md:text-xl dark:text-white"> <?php echo $opinion->firstName. " ". $opinion->lastName;  ?> </p>
+                        <p class="text-center mt-5 px-3 font-semibold italic leading- dark:text-white"> <span class="text-gray-400 dark:text-gray-600 text-3xl">&ldquo;</span> <?php echo $opinion->content; ?> <span class="text-gray-400 dark:text-gray-600 text-3xl">&rdquo;</span></p>
+                            <div class="flex mt-4 md:mt-5 lg:mt-10 mb-5 justify-center gap-1">
 
-            <div class="hidden  overflow-y-auto duration-700 ease-in-out bg-white border border-gray-300 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" data-carousel-item="active">
-                <div class="flex justify-center items-center mt-10">
-                    <img src="./assets/clientProfile/unknownPP.jpg" class="md:w-20 w-14 rounded-full object-cover">
-                </div>
-                <p class="text-center mt-5 font-semibold md:text-xl dark:text-white">Benyounes Mohammed Anis</p>
-                <p class="text-center mt-5 px-3 font-semibold italic leading- dark:text-white"> <span class="text-gray-400 dark:text-gray-600 text-3xl">&ldquo;</span> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, doloribus. <span class="text-gray-400 dark:text-gray-600 text-3xl">&rdquo;</span></p>
-                    <div class="flex mt-4 md:mt-5 lg:mt-10 mb-5 justify-center gap-1">
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
-                        </svg>
-                        
+                                <?php 
+                                    for ($i ; $i < $ratings ; $i++) { ?>
+                                        <svg class="w-8 h-8 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+                                        </svg>
+                                    <?php }
+                                ?>
+                                
+                            </div>
                     </div>
-            </div>
-           
+
+                <?php }
+            ?>
+
+            
 
         </div>
         
