20e7e1a20838d0ce5a6e6ab432ab6838cd22d23b
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-04-19 17:11:51 +0200

    Add The Charts Section

--- views/admin/dashboardSide.php
+++ views/admin/dashboardSide.php
@@ -11,17 +11,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../node_modules/flowbite/dist/flowbite.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     
 
         <?php include '../components/sidebar.php' ; ?>
         
-        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 h-screen" id="content-container">
-            <p class="mb-3 text-2xl font-semibold text-gray-900 dark:text-white">Dashboard</p>
+        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 " id="content-container">
+            <p class=" text-2xl font-semibold text-gray-900 dark:text-white">Dashboard</p>
 
            <?php include '../components/dashboardCardsSection.php' ?>
 
+        </div>
+
+        <!-- CHARTS -->
+        <div id="chartsContainer" class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 flex flex-col h-screen">
+
+            <div class="w-full">
+                <?php include '../components/charts/vehiclesCountByBrandChart.php' ?>
+            </div>
+                    
         </div>
 
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
