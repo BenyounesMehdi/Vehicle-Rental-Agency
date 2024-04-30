23200f9aaefd358fd3391ec142171cd6f4ba038f
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-04-30 19:49:03 +0200

    UPD The UI

--- views/components/rating.php
+++ views/components/rating.php
@@ -8,7 +8,7 @@
        $opinions = $stmt->fetchAll();
 
     //    var_dump($opinions) ;
-
+        $table = 'opinion' ;
        $totalOpinions = countRableRows($pdo, $table) ;
     //    echo $totalOpinions ;
 
@@ -20,9 +20,9 @@
    ?>
 
 
-<div class="bg-gray-900 dark:bg-gray-600 w-1/2 px-2 py-3 rounded-lg">
+<div class="bg-white dark:bg-gray-800 w-full px-2 py-3 rounded-lg">
     <div class="flex items-center justify-between mb-2">
-            <p class="text-white text-4xl font-semibold ml-2">Ratings</p>
+            <p class="text-black dark:text-white text-4xl font-semibold ml-2">Ratings</p>
             <div class="flex items-center mb-0 justify-between mr-2 ">
                 <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
@@ -33,9 +33,9 @@
             </div>
         </div>
 
-        <p class="text-sm font-medium text-gray-500 dark:text-gray-400"><?php echo $totalOpinions; ?> global ratings</p>
+        <p class="text-sm ml-2 font-medium text-gray-500 dark:text-gray-400"><?php echo $totalOpinions; ?> global ratings</p>
 
-        <div class="flex flex-col justify-start items-center mt-1 mb-2">
+        <div class="flex flex-col justify-start items-center ml-2 mt-1 mb-2">
             <?php foreach ($opinions as $opinion): ?>
                 <div class="flex items-center mt-4  w-full">
                     <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline"><?php echo $opinion->rating; ?> star</a>
