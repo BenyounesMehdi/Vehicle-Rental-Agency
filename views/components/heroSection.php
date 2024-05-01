f4b0bda58373279be8bdb79d549a3a01e7ab95a5
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-01 17:03:19 +0200

    Finished The UI

---
+++ views/components/heroSection.php
@@ -0,0 +1,8 @@
+<div class="bg-gray-200 dark:bg-gray-800  flex justify-center items-center p-2">
+    <div class="w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold px-4">
+        <p class="dark:text-white">Looking for a vehicle? <span class="text-blue-700">You’re at the right place.</span></p>
+    </div>
+    <div class="w-1/2 mb-7">
+        <img src="./assets/others/heroSectionImage.png" >
+    </div>
+</div>
