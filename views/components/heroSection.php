fade1608490a2f71c306d341cd0f3152e6ffdcf3
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-02 00:39:48 +0200

    UPD The UI

--- views/components/heroSection.php
+++ views/components/heroSection.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-200 dark:bg-gray-800  flex justify-center items-center p-2">
-    <div class="w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold px-4">
+    <div class="mb-7 w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold px-4">
         <p class="dark:text-white">Looking for a vehicle? <span class="text-blue-700">You’re at the right place.</span></p>
     </div>
     <div class="w-1/2 mb-7">
