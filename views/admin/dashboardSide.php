3c04889558e95e5d7240739fddf315ec19fe68e7
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-04 01:02:11 +0200

    UPD The Ui & ADD The Count Vehicles Chart

--- views/admin/dashboardSide.php
+++ views/admin/dashboardSide.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../node_modules/flowbite/dist/flowbite.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
@@ -23,43 +24,51 @@
            <?php include '../components/dashboardCardsSection.php' ?>
         </div>
 
+        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900">
+            <?php include '../components/todayReservations.php'; ?>
+        </div>
             
         <div class="flex flex-col lg:flex-row bg-gray-300 dark:bg-gray-900 p-4 sm:ml-64 justify-center items-center gap-2 ">
             <div id="rating" class="w-full lg:w-1/2 ">
                 <?php include '../components/rating.php'; ?>
             </div>
 
-            <div id="vehiclesStatus" class="w-full lg:w-1/2 ">
-                <?php include '../components/vehicleStatusStats.php'; ?>
+            <div id="vehiclesStatus" class="w-full lg:w-1/2 flex flex-col">
+                <div>
+                    <?php include '../components/vehicleStatusStats.php'; ?>
+                </div>
+                
             </div>
         </div>
 
-        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900">
-             <?php include '../components/clientsStats.php'; ?>
-        </div>
-
-        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900">
-             <?php include '../components/reservationsReport.php'; ?>
-        </div>
-
-        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900">
+        <div class=" p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 grid grid-cols-1 lg:grid-cols-2 gap-2">
+            <div class="">
+                <?php include '../components/clientsStats.php'; ?>
+            </div>
             
-            <?php include '../components/todayReservations.php'; ?>
-        </div>
-
-        <!-- CHARTS -->
-        <div id="chartsContainer" class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 flex flex-col h-screen">
-
-            <div class="w-full">
-                <?php include '../components/charts/vehiclesCountByBrandChart.php' ?>
+            <div class="">
+                <?php include '../components/countVehicles.php' ; ?>
             </div>
-                    
         </div>
+
+        <div class="p-4 sm:ml-64 bg-gray-300 dark:bg-gray-900 ">
+            <div>
+                <?php include '../components/reservationsReport.php'; ?>
+            </div>
+        </div>
+        
+        
+        
 
         
 
+        
+        
+        
+        
+
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    
     <script src="../JS/themeToggle.js"></script>
     <script src="../JS/arrowToggle.js"></script>
     <script>
