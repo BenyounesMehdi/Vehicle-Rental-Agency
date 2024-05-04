00346420c7ad461011f33a3e7de7e1c6634d4352
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-04 15:31:32 +0200

    UPD The Ui

--- views/components/clientsStats.php
+++ views/components/clientsStats.php
@@ -25,7 +25,7 @@
     // echo "Percentage of clients with no reservations: " . round($clientsWithNoReservationsPercentage, 2) . "%";
 ?>
 
-<div class=" w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 ">
+<div class=" w-full bg-white rounded-lg shadow dark:bg-gray-800 px-4 py-3.5 ">
 
    <div class="flex justify-between items-start w-full">
 
