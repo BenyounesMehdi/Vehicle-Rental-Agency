4d5a729417caf97e32b53b0044dcf4a38c1ae1f6
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-19 15:04:12 +0200

    Handle The Edit Reservation Process

---
+++ models/backend/admin/editReservation.php
@@ -0,0 +1,27 @@
+<?php
+
+if( isset($_POST['editReservation']) ) {
+    $reservationID = $_POST['reservationID'] ;
+    $pickupDate = date('Y-m-d', strtotime($_POST['pickupDate']))  ;
+    $returnDate = date('Y-m-d', strtotime($_POST['returnDate'])) ;
+    $duration = $_POST['duration'] ;
+    $totalCost = $_POST['totalCost'] ;
+    $reservationDate = date('Y-m-d');
+    $isPayed = 'no';
+
+    require_once '../../database.php' ;
+    
+    $query = "UPDATE reservation 
+      SET pickupDate = ?, returnDate = ?, duration = ?, totalCost = ?
+      WHERE reservationID = ?";
+      $stmt = $pdo->prepare($query);
+      $updated = $stmt->execute([$pickupDate, $returnDate, $duration, $totalCost, $reservationID]);
+
+    if( $updated ) {
+        echo "Done" ;
+        header( 'location: ../../../views/admin/reservationsSide.php' ) ;
+    }
+    else {
+        echo "Fail" ;
+    }
+}
