5169e1f420a91226dd07a0b26a3d35b1d81e1638
Benyounes Mehdi <benyounesmahdi1234@gmail.com>
2024-05-19 14:45:21 +0200

    ADD The Action Buttons

--- views/reservations/reservationsList.php
+++ views/reservations/reservationsList.php
@@ -1,4 +1,9 @@
 <?php 
+
+        $currentDate = new DateTime('now');
+        $currentDate->setTime(0, 0, 0); // Set time to midnight
+
+
     // require_once '../../models/database.php' ;
         $query = 'SELECT r.*, v.name as vehicleName, v.image as image, b.name as brandName,
                     c.firstName as firstName, c.lastName as lastName 
@@ -50,6 +55,9 @@
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Status
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
                     </th>
                 </tr>
             </thead>
@@ -103,8 +111,48 @@
                                 <td class="px-6 py-2 font-bold <?php echo ($reservation->isExpired == 'No') ? 'text-red-500' : 'text-green-500'  ?> ">
                                     <?php echo ($reservation->isExpired == "Yes") ?  "Expired" : "Not Expired" ; ?>
                                 </td>
+
+                                <td class="px-6 py-2">
+                                            <?php if( $reservation->isExpired == 'No' ) { 
+                                                    $pickupDate = new DateTime($reservation->pickupDate);
+                                                    $pickupDate->setTime(0, 0, 0); // Set time to midnight
+
+                                                   
+                                                    if( $pickupDate > $currentDate ) {
+                                                ?>
+                                            <a href="../reservations/editReservation.php?reservation=<?php echo $reservation->reservationID ?>&vehicle=<?php echo $reservation->vehicleID ?>">
+                                                <button type="button" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 ">Edit</button>    
+                                            </a>
+                                            <a data-modal-target="popup-modal" data-modal-toggle="popup-modal" >
+                                            <button onclick="storeID(<?php echo $reservation->reservationID; ?>, <?php echo $reservation->vehicleID; ?>)" type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600">Delete</button>
+                                            </a>
+                                            <?php } } ?>
+                                </td>
                                 
                             </tr>
+
+                                    <div id="popup-modal" tabindex="-1" class="hidden fixed inset-0 overflow-y-auto overflow-x-hidden z-50  h-[calc(100%-1rem)] max-h-full" >
+                                        <div class="relative p-4 w-full max-w-md max-h-full">
+                                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                
+                                                <div class="p-4 md:p-5 text-center rounded border shadow-xl border-gray-500">
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                                    </svg>
+                                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this item?</h3>
+                                    
+                                                    
+                                                    <a id="deleteLink" href="#" >
+                                                        <button name="deleteButton" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                                Yes, I'm sure
+                                                            </button>
+                                                    </a>
+                                                    
+                                                    <button id="cancelButton" onclick="closeModel()" data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-200 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                             
 
                         <?php }
@@ -114,3 +162,19 @@
 
         </table>
     </div>
+
+
+<script>
+
+    function storeID (reservationID, vehicleID) {
+
+        var deleteUrl = "../../models/backend/client/deleteReservation.php?id=" + reservationID +"&vehicle=" + vehicleID;
+        
+        // Get the anchor tag by ID
+        var deleteLink = document.getElementById("deleteLink");
+        
+        // Set the href attribute of the anchor tag
+        deleteLink.href = deleteUrl;
+    }
+
+</script>
